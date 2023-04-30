1a735cfe32550089c9357f3aae2932959cfad1a1
Alexandru Meta <116649660+alex-meta-dev@users.noreply.github.com>
2023-04-30 18:46:06 +0300

    added main functions for searchBar and tableView

---
+++ PokemonApp/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  PokemonApp
+//
+//  Created by Alexandru Meta on 30.04.2023.
+//
+
+import Foundation
+
--- PokemonApp/ViewController.swift
+++ PokemonApp/ViewController.swift
@@ -4,10 +4,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    // Important stuff
+    var nameOfPokemonsList = [String]()
+    
+    var searchList = [String]()
+    var isSearching = false
+    
+    var pokemonDetailed = PokemonModel(base_experience: 1, id: 1, height: 1, weight: 1, order: 1)
+    
+    var listOfPokemons = [Pokemon]()
+    
+    
+    // Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var pokemonTableView: UITableView!
+    
+    
+    // ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // Fetching data from the API functions
+    
+    func fetchDataFromApiAndDecode() {
+        
+    }
+    
+    func fetchDataFromIndividualPokemonLink() {
+        
+    }
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        <#code#>
+    }
+}
