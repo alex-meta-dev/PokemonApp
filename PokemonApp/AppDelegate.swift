a2342f8efc083363a0a1105243c606c9ecaca14b
Alexandru Meta <116649660+alex-meta-dev@users.noreply.github.com>
2023-04-30 16:47:50 +0300

    Initial refactor

--- PokemonApp/AppDelegate.swift
+++ PokemonApp/AppDelegate.swift
@@ -1,9 +1,6 @@
-//
 //  AppDelegate.swift
 //  PokemonApp
-//
 //  Created by Alexandru Meta on 30.04.2023.
-//
 
 import UIKit
 
--- PokemonApp/SceneDelegate.swift
+++ PokemonApp/SceneDelegate.swift
@@ -1,9 +1,6 @@
-//
 //  SceneDelegate.swift
 //  PokemonApp
-//
 //  Created by Alexandru Meta on 30.04.2023.
-//
 
 import UIKit
 
